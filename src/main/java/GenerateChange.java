4d9868ac26c3a04130bcfc24dc8b359757dda26d
Megan Fayer and Michael Gallegos <mkgallegos@gmail.com>
2016-02-11 09:28:28 -0800

    pass tiny specs.

--- src/main/java/GenerateChange.java
+++ src/main/java/GenerateChange.java
@@ -5,36 +5,58 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class GenerateChange {
-  public static void main(String[] args) {
+  public static void main(String[] args) {}
+//
 //     String layout = "templates/layout.vtl";
-
+//
 //     get("/", (request, response) -> {
 //       Map<String, Object> model = new HashMap<String, Object>();
 //       model.put("template", "templates/home.vtl");
 //       return new ModelAndView(model, layout);
 //     }, new VelocityTemplateEngine());
-
+//
 //     get("/detector", (request, response) -> {
 //       Map<String, Object> model = new HashMap<String, Object>();
 //       model.put("template", "templates/detector.vtl");
-
+//
 //       String year = request.queryParams("year");
 //       Integer integerYear = Integer.parseInt(year);
 //       Boolean isLeapYear = isLeapYear(integerYear);
-
+//
 //       model.put("isLeapYear", isLeapYear);
 //       model.put("year", request.queryParams("year"));
 //       return new ModelAndView(model, layout);
 //     }, new VelocityTemplateEngine());
 //   }
 
-//   public static Boolean isLeapYear(Integer year) {
-//     if ( year % 400 == 0 ) {
-//       return true;
-//     } else if ( year % 100 == 0 ) {
-//       return false;
-//     } else {
-//       return year % 4 == 0;
-//     }
+  public static String generateChange (Integer inputInteger) {
+
+    Integer input = inputInteger;
+    Integer quarterCount = 0;
+    Integer dimeCount = 0;
+    Integer nickelCount = 0;
+    String result = "";
+
+    while (input >= 25) {
+      input -= 25;
+      quarterCount++;
+      result += quarterCount + " quarter.";
+    }
+    while (input >= 10) {
+      input -= 10;
+      dimeCount++;
+      result += " " + dimeCount + " dime.";
+    }
+    while (input >= 5) {
+      input -= 5;
+      nickelCount++;
+      result += " " + nickelCount + " nickel.";
+    }
+
+    if (input != 0) {
+      result += " And " + input + " penny.";
+    }
+
+    return result;
   }
 }
--- src/test/java/GenerateChangeTest.java
+++ src/test/java/GenerateChangeTest.java
@@ -1,10 +1,27 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 
-public class ChangeGeneratorTest {
+public class GenerateChangeTest {
 
   @Test
-  public void method_input_result() {
+  public void generateChange_AmountOfMoneyInCents_quarters() {
+    GenerateChange generateChangeTest = new GenerateChange();
+    assertEquals("1 quarter." , generateChangeTest.generateChange(25));
+  }
+  @Test
+  public void generateChange_AmountOfMoneyInCents_dimes() {
+    GenerateChange generateChangeTest = new GenerateChange();
+    assertEquals("1 quarter. 1 dime." , generateChangeTest.generateChange(35));
+  }
 
+  @Test
+  public void generateChange_AmountOfMoneyInCents_nickels() {
+    GenerateChange generateChangeTest = new GenerateChange();
+    assertEquals("1 quarter. 1 dime. 1 nickel." , generateChangeTest.generateChange(40));
+  }
+  @Test
+  public void generateChange_AmountOfMoneyInCents_pennies() {
+    GenerateChange generateChangeTest = new GenerateChange();
+    assertEquals("1 quarter. 1 dime. 1 nickel. And 1 penny." , generateChangeTest.generateChange(41));
   }
 }
